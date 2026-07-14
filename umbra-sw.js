47dc419f1a122f7674fe7bd8d4b32cacc5de1d17
k4nb5tk4nd-ship-it <k4nb5tk4nd@privaterelay.appleid.com>
2026-07-14 09:43:57 +0200

    Add files via upload

--- umbra-sw.js
+++ umbra-sw.js
@@ -1,4 +1,4 @@
-const CACHE='eclipso-v17';
+const CACHE='eclipso-v18';
 const CORE=['./','./index.html','./eclipso-totality-2026.js','./astronomy.browser.min.js','./umbra-manifest.webmanifest','./umbra-icon-180.png','./umbra-icon-192.png','./umbra-icon-512.png','./umbra-icon-1024.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
